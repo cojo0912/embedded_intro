4e1df07ed3b86b863ab36dea66c23b6dfa125a26
Andrei <andrei.cojocaru@radcom.ro>
2022-06-26 15:37:54 +0300

    .

---
+++ 1_intro/basic_intro/main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+//#include <math.h>
+using namespace std;
+#define TEST 5
+
+// bit byte/octet memorie
+// INTERFATA se numeste IDE
+// COD -> COMPILATOR -> executabil/binar (limbaj cod masina)
+// 1 bit -> 0/1 -> false/true 
+// 1 octet/byte = 8 biti -> 00000000, 00000001
+// https://mquest.ro/home/learnunit?id=21
+
+//0000 0000 -> 0
+//1 111 1111 -> 255 : 1 octet
+
+//char 1 byte, short int 2 byte, int 4 byte, long 8 byte
+//unsinged char
+
+// tipuri de date 
+// reprezentarea tipurilor de date
+
+// 1101 =
+// 1 * 2^0 +
+// 0 * 2^1 +
+// 1 * 2^2 +
+// 1 * 2^3 +
+// = 1 + 0 + 4 + 8 = 13
+
+// https://ocw.cs.pub.ro/courses/programare/laboratoare/lab02
+// cpp.sh
+
+
+
+int main()
+{
+    int x;
+    scanf("%d", &x);    
+
+
+
+    getch();
+    return 0;
+}
