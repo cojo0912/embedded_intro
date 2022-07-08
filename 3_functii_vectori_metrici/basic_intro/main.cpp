cb5698ddd9a417f06893803f687d49c654e293a5
andrei.cojocaru <andrei@MacBook-Pro-2.local>
2022-07-08 15:08:41 +0300

    .

---
+++ 3_functii_vectori_metrici/basic_intro/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+
+
+// https://ocw.cs.pub.ro/courses/programare/laboratoare/lab04
+// https://ocw.cs.pub.ro/courses/programare/laboratoare/lab05
+// https://ocw.cs.pub.ro/courses/programare/laboratoare/lab06
+// cpp.sh
+
+int main()
+{
+    
+    
+
+    getch();
+    return 0;
+}
